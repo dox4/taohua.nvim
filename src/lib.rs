1623ecccc851e55284f1d3e8801d21dc9090898c
dox4 <dox4@foxmail.com>
2026-05-22 17:46:45 +0800

    refactor: return structured parse results from plugin

--- src/lib.rs
+++ src/lib.rs
@@ -1,29 +1,59 @@
-use nvim_oxi::Result;
-use nvim_oxi::serde::{DeserializeError, Serializer};
+use nvim_oxi::serde::Serializer;
 use nvim_oxi::{Dictionary, Function, Object};
 use serde::Serialize;
 use std::fs;
 
 #[nvim_oxi::plugin]
-fn taohua() -> Dictionary {
-    let parse_fn: Function<String, Result<Object>> = Function::from_fn(parse_toml);
+pub fn taohua() -> Dictionary {
+    let parse_fn: Function<String, Dictionary> = Function::from_fn(parse_toml);
     Dictionary::from_iter([("parse_toml", parse_fn)])
 }
 
-fn parse_toml(filepath: String) -> Result<Object> {
-    let contents = fs::read_to_string(&filepath).map_err(|e| {
-        nvim_oxi::api::Error::Other(format!("Failed to read file {}: {}", filepath, e))
-    })?;
+fn parse_toml(filepath: String) -> Dictionary {
+    let contents = match fs::read_to_string(&filepath) {
+        Ok(contents) => contents,
+        Err(err) => {
+            return Dictionary::from_iter([
+                ("ok", Object::from(false)),
+                (
+                    "error",
+                    Object::from(format!("Failed to read file {}: {}", filepath, err)),
+                ),
+            ]);
+        },
+    };
 
-    let toml_value: toml::Value = toml::from_str(&contents).map_err(|e| {
-        nvim_oxi::Error::Deserialize(DeserializeError::Custom {
-            msg: format!("Failed to parse TOML file {} with error: {}", filepath, e),
-        })
-    })?;
+    let toml_value: toml::Value = match toml::from_str(&contents) {
+        Ok(value) => value,
+        Err(err) => {
+            return Dictionary::from_iter([
+                ("ok", Object::from(false)),
+                (
+                    "error",
+                    Object::from(format!(
+                        "Failed to parse TOML file {} with error: {}",
+                        filepath, err
+                    )),
+                ),
+            ]);
+        },
+    };
 
-    let obj = toml_value
-        .serialize(Serializer::new())
-        .map_err(|e| nvim_oxi::Error::ObjectConversion(e.into()))?;
+    let obj = match toml_value.serialize(Serializer::new()) {
+        Ok(obj) => obj,
+        Err(err) => {
+            return Dictionary::from_iter([
+                ("ok", Object::from(false)),
+                (
+                    "error",
+                    Object::from(format!(
+                        "Failed to serialize TOML file {} with error: {}",
+                        filepath, err
+                    )),
+                ),
+            ]);
+        },
+    };
 
-    Ok(obj)
+    Dictionary::from_iter([("ok", Object::from(true)), ("value", obj)])
 }
